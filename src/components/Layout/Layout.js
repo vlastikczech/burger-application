f1739659d4115d2be7d443d910eecdd1fadcd4f1
vlastik <vlastikczech1@gmail.com>
2018-07-22 22:29:54 -0700

    created the Layout Component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Aux from '../..//hoc/_Aux';
+
+const layout = (props) => (
+    <Aux>
+      <div>ToolBar, SideDrawer, Backdrop</div>
+        <main>
+        {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
