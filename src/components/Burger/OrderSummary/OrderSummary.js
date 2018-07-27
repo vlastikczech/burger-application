47a20a6211d5ccc7b8798516ecc6aba67c6d0f61
vlastik <vlastikczech1@gmail.com>
2018-07-26 19:02:16 -0700

    created the backdrop component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -14,7 +14,7 @@ const orderSummary = (props) => {
     return(
         <Aux>
             <h3>You Order</h3>
-            <p>A delicious bruger with the foloowing ingredients:</p>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
